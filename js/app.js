deef291d15020e043156ecd3517c92f97927fd14
aamL <k.johne90@web.de>
2017-02-09 03:07:51 +0100

    add manuel styling trough Input Elements

--- js/app.js
+++ js/app.js
@@ -1,16 +1,29 @@
 var app = angular.module('app', []);
 
+/**
+ * General Controller to prepare options for Directives
+ */
 app.controller('appController', function ($http, $scope) {
     this.template = '/templates/adItem.html';
+
+    this.style = {};
+
+    // used in template
     this.submit = function() {
         $scope.$broadcast('updateAdvert', this.template );
     };
 });
 
+/**
+ * Directive to collect Data, and server as an API for children directives to get Advert generated HTML code
+ */
 app.directive('advertScreen', function ($http, $compile, $q, $log, $templateRequest) {
     return {
         restrict: 'E',
         require: 'advertScreen',
+        scope: {
+            'style' : '=ngStyle'
+        },
         controller: function ($scope) {
             var template;
             var html;
@@ -44,7 +57,8 @@ app.directive('advertScreen', function ($http, $compile, $q, $log, $templateRequ
                     html.querySelector('head').appendChild(style);
                 }).catch(function(e) {
                     $log.warn( 'style couldnt be fetched' );
-                    $log.info( 'Maybe anti ad is running, please deactivate');
+                    $log.info( 'Your anti ad software is running, please deactivate. For full experience' );
+                    alert( 'Your anti ad software is running, please deactivate. For full experience' );
                     $log.warn( e );
                 });
             };
@@ -67,6 +81,10 @@ app.directive('advertScreen', function ($http, $compile, $q, $log, $templateRequ
     }
 });
 
+/**
+ * Directive to collect HTML and serve the specific View option of the HTML
+ * by setting scope.codeSnipped [NODE}
+ */
 app.directive('advertView', function () {
     return {
         restrict: 'E',
@@ -78,7 +96,11 @@ app.directive('advertView', function () {
     }
 });
 
-app.directive('viewPreview', function () {
+/**
+ * specific directive to render HTML in iFrame
+ * needs scope.codeSnipped {Node}
+ */
+app.directive('viewPreview', function ( $timeout ) {
     return {
         restrict: 'A',
         template: '<iframe scrolling="no"></iframe>',
@@ -87,7 +109,7 @@ app.directive('viewPreview', function () {
             var iFrame = element[0].contentWindow;
             scope.$watch( 'codeSnipped', function( newArray ){
                 // need timeout otherwise get pre compiled html
-                setTimeout( function(){
+                $timeout( function(){
                     if( scope.codeSnipped && scope.codeSnipped.outerHTML ) {
                         iFrame.document.open();
                         iFrame.document.write( newArray.outerHTML );
@@ -99,10 +121,65 @@ app.directive('viewPreview', function () {
     }
 });
 
+/**
+ * specific directive to display HTML code
+ * needs scope.codeSnipped {Node}
+ */
 app.directive('viewHtml', function() {
     return {
         restrict: 'A',
         template: '<textarea readonly>{{ codeSnipped.outerHTML }}</textarea>',
         replace: true
+    }
+});
+
+/**
+ * directive to apply inline style to advert
+ * needs scope.style {Object}
+ *
+ * @supports
+ * css selector
+ * css styles ( font-weight = fontWeight )
+ * css values
+ *
+ * @example
+ * style : {
+ *      '.IamASelector' : {
+ *          'backgroundColor' : 'black',
+ *          'color' : 'white'
+ *      }
+ * }
+ */
+app.directive('advert', function( $timeout ) {
+    return {
+        restrict: 'C',
+        link: function( scope, element ) {
+            var elementSelector;
+            var targetElement;
+            var styleValue;
+            var styleProperty;
+
+            // tried scope.$evalAsync to run at least on digest circle
+            // however waiting for one digest and rendering works too
+            $timeout( function() {
+                // O(n)^3
+                for( elementSelector in scope.style ) { // iterate trough all styled elements
+                    if( scope.style.hasOwnProperty( elementSelector ) ) { // just checking if its own property
+                        targetElement = element[0].querySelectorAll( elementSelector ); // apply selector to find all elements
+                        if( targetElement.length > 0 ) {
+                            for( styleProperty in scope.style[ elementSelector ] ) {  // iterate trough all properties
+                                if ( scope.style[ elementSelector ].hasOwnProperty( styleProperty ) ) { // just checking if its own property
+                                    styleValue = scope.style[ elementSelector ][ styleProperty ]; // get value from property
+
+                                    Array.prototype.forEach.call( targetElement, function( target ) { // walking trough nodeList
+                                        target.style[ styleProperty ] = styleValue; // assign value of property to element
+                                    });
+                                }
+                            }
+                        }
+                    }
+                }
+            });
+        }
     }
 });
